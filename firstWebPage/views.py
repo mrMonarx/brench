02d34ec5977b6215d1d2681b4bb15ff628f4ddbf
Baxriddin <turgunpolatovbaxriddin@gmail.com>
2022-06-22 12:26:44 +0500

    installed apps

--- firstWebPage/views.py
+++ firstWebPage/views.py
@@ -2,17 +2,20 @@ from django.http import HttpResponse
 from django.utils import timezone
 from random import randint
 
+from articles.models import Article
+
 
 
 def home(request):
     name = 'Django'
-    number = randint(1,100)
+    number = randint(1,2)
     date = timezone.now()
-    # databae =
-    # title =
-    # content = 
-    H1_STRING = f"""<h1>Hello {name},Time:{date}</h1>"""
-    P_STRING = f"""<p>Hello {number}</p>"""
+    # database =
+    obj = Article.objects.get(id=number)
+    title = obj.title
+    content = obj.content
+    H1_STRING = f"""<h1>Hello {title} ,({number}) </h1>"""
+    P_STRING = f"""<p>Hello {content}</p>"""
     HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
 
