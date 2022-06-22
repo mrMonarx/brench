fb15674beec3375ed4b05ab70b8b9b3fc2ba2ffa
Baxriddin <turgunpolatovbaxriddin@gmail.com>
2022-06-22 11:34:39 +0500

    Update views.py

--- firstWebPage/views.py
+++ firstWebPage/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.utils import timezone
+from random import randint
 
-Home_Web_View = """Python is the best programming language"""
 
 
 def home(request):
-    print(Home_Web_View)
-    return HttpResponse(Home_Web_View)
+    name = 'Django'
+    number = randint(1,100)
+    date = timezone.now()
+    H1_STRING = f"""<h1>Hello {name},Time:{date}</h1>"""
+    P_STRING = f"""<p>Hello {number}</p>"""
+    HTML_STRING = H1_STRING + P_STRING
+    return HttpResponse(HTML_STRING)
+
+
