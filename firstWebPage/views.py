98b6834472dd4f567a150253f8a93bc230e9f627
Baxriddin <turgunpolatovbaxriddin@gmail.com>
2022-06-22 11:46:49 +0500

    add app

--- firstWebPage/views.py
+++ firstWebPage/views.py
@@ -8,6 +8,9 @@ def home(request):
     name = 'Django'
     number = randint(1,100)
     date = timezone.now()
+    # databae =
+    # title =
+    # content = 
     H1_STRING = f"""<h1>Hello {name},Time:{date}</h1>"""
     P_STRING = f"""<p>Hello {number}</p>"""
     HTML_STRING = H1_STRING + P_STRING
