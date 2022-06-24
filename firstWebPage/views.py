008201bee473097c133545399cb8f6575baa48f0
Baxriddin <turgunpolatovbaxriddin@gmail.com>
2022-06-24 12:14:05 +0500

    data_list

--- firstWebPage/views.py
+++ firstWebPage/views.py
@@ -12,15 +12,20 @@ def home(request):
     date = timezone.now()
     # database =
     obj = Article.objects.get(id=number)
+    my_list = ['python','django','flask','django rest api','flask api','c++','c#','go']
     # title = obj.title
     # content = obj.content
 
+    objects = Article.objects.all()
+
     content = {'title': obj.title,
                'id': obj.id,
-               'content': obj.content}
+               'content': obj.content,
+               'my_list': my_list,
+               'objects': objects}
 
 
-    HTML_STRING = render_to_string(template_name='my_footer.html', context=content)
+    HTML_STRING = render_to_string(template_name='home.html', context=content)
     return HttpResponse(HTML_STRING)
 
 
